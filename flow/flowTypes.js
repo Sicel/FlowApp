652dddbc0c70a5df404a1bea5bec3ce4466a17e4
Cristian <cxc9597@rit.edu>
2021-04-04 20:39:24 -0400

    Heroku deploy?

---
+++ flow/flowTypes.js
@@ -0,0 +1,39 @@
+// import * as t from "@onflow/types";
+
+const t = require("@onflow/types");
+
+exports.types = {
+    "Identity" : t.Identity,
+    "UInt" : t.UInt,
+    "Int" : t.Int,
+    "UInt8" : t.UInt8,
+    "Int8" : t.Int8,
+    "UInt16" : t.UInt16,
+    "Int16" : t.Int16,
+    "UInt32" : t.UInt32,
+    "Int32" : t.Int32,
+    "UInt64" : t.UInt64,
+    "Int64" : t.Int64,
+    "UInt128" : t.UInt128,
+    "Int128" : t.Int128,
+    "UInt256" : t.UInt256,
+    "Int256" : t.Int256,
+    "Word8" : t.Word8,
+    "Word16" : t.Word16,
+    "Word32" : t.Word32,
+    "Word64" : t.Word64,
+    "UFix64" : t.UFix64,
+    "Fix64" : t.Fix64,
+    "String" : t.String,
+    "Character" : t.Character,
+    "Bool" : t.Bool,
+    "Address" : t.Address,
+    "Void" : t.Void,
+    "Optional" : t.Optional,
+    "Reference" : t.Reference,
+    "Array" : t.Array,
+    "Dictionary" : t.Dictionary,
+    "Event" : t.Event,
+    "Resource" : t.Resource,
+    "Struct" : t.Struct
+}
---
+++ index.js
@@ -0,0 +1,54 @@
+const flow = require("./flow/flow.js");
+const t = require("./flow/flowTypes.js");
+
+const express = require("express");
+const app = express();
+
+let accountAddr;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Connected to Unity");
+});
+
+app.get("/flow", (req, res) => {
+    
+})
+
+app.get("/flow/type/", (req, res) => {
+    res.send(t.types["Address"]);
+})
+
+app.post("/flow/typeTest/", (req, res) => {
+    const type = req.body.type;
+
+    console.log(req.body);
+
+    res.send(t.types[req.body.type])
+})
+
+app.post("/flow/user/", async (req, res) => {
+    res.send(await flow.getUser(req.body.address));
+})
+
+// Sends transaction using request from Unity
+app.post("/flow/transaction/", async(req, res) => {
+    let response;
+    let txReq = req.body;
+    accountAddr = txReq.address;
+
+    try {
+        const user = await flow.getAuthorization(accountAddr);
+        response = await flow.sendTxReq(user, txReq);
+    } catch (error) {
+        response = error;
+    }
+
+    console.log("---------------\nResponse:\n" + response);
+    res.send(response);
+});
+
+app.listen(300, () => { 
+    console.log("-----------------------------------------\nWorking as intended");
+});
